4c16c9a8ead446e56913a0bdf6b92a89d163619c
German Antonio <german@tiralinea.com>
2021-03-12 05:24:07 -0300

    Added validation

--- services/is-prime.js
+++ services/is-prime.js
@@ -4,9 +4,14 @@
  * @returns {Boolean} Wether the number is prime
  */
 function isPrime(number) {
+  if (typeof number !== 'number') {
+    throw new TypeError('Input must be a number');
+  }
+
   for (let i = 2; i < number; i++) {
     if (number % i === 0) return false;
   }
+
   return number > 1;
 }
 
