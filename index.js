a9d5f11d2f81e841c70acaade8a24a06c0ff3f3e
German Antonio <german@tiralinea.com>
2021-03-12 19:55:14 -0300

    Beautified index

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 /* Dependencies */
-const { prompt } = require('inquirer');
-const path = require('path');
-require('colors');
+const { prompt } = require("inquirer");
+const path = require("path");
+require("colors");
 
 /* Services */
-const getPrimeNumbers = require(path.join(process.cwd(), 'services', 'get-prime-numbers'))
+const getPrimeNumbers = require(path.join(
+  process.cwd(),
+  "services",
+  "get-prime-numbers"
+));
 
-console.log('\n-- Technysis code challenge --\n'.cyan.bold);
-console.log('\nGet a descending list of prime numbers starting from a max number\n'.cyan.bold);
+console.log("\n-- Technysis code challenge --\n".cyan.bold);
+console.log(
+  "\nGet a descending list of prime numbers starting from a max number\n".cyan
+    .bold
+);
 
 /**
  * Prompt the user for a number between 2 and 100000, then print
  * a descending list of all prime numbers starting from it
  */
 (async () => {
-  const { maxNumber } = await prompt([
-    {
-      type:'number',
-      name: 'maxNumber',
-      message: 'Please type the max number:  '.bold.white,
-    }
-  ]);
+  while (true) {
+    try {
+      const { maxNumber } = await prompt([
+        {
+          type: "number",
+          name: "maxNumber",
+          message: "Please type a number: ".white.bold + "(ctrl+C to exit) ".gray.dim,
+        },
+      ]);
 
-  try {
-    const numbers = getPrimeNumbers(maxNumber);
-    for (const number of numbers) {
-      console.log(number);
+      const numbers = getPrimeNumbers(maxNumber);
+      console.log(`Prime numbers below ${maxNumber} are:`.green.bold);
+      let printNumbers = '';
+
+      for (const number of numbers) {
+        printNumbers += `${number}, `
+      }
+      console.log(printNumbers.slice(0, -2).yellow);
+    } catch (error) {
+      console.log(error.message.red.bold);
     }
-    process.exit(0);
-  } catch (error) {
-    console.dir(error.message);
-    process.exit(1);
   }
 })();
