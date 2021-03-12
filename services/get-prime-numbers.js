947c5ac708951def00a450566ba7d87a8e05b7ed
German Antonio <german@tiralinea.com>
2021-03-12 03:57:13 -0300

    Added validation plus minor formatting fixes

--- services/get-prime-numbers.js
+++ services/get-prime-numbers.js
@@ -2,20 +2,22 @@
 const path = require('path');
 
 /* Services */
+const validateInput = require(path.join(process.cwd(), 'services', 'validate-input'));
 const isPrime = require(path.join(process.cwd(), 'services', 'is-prime'));
 
 /**
- * Gets a desc. list of all prime numbers between 2 and a given number (defaults to 0)
+ * Gets a desc. list of all prime numbers between 2 and a given number
  * @param {Number} topNumber The max number in the range to print
  * @returns {Number[]} An array of prime numbers
  */
 function getPrimeNumbers(topNumber) {
+  validateInput(topNumber);
   const result = [];
+
   for (let currNumber = topNumber; currNumber > 1; currNumber--) {
-    if (isPrime(currNumber)) {
-      result.push(currNumber);
-    }
+    if (isPrime(currNumber)) result.push(currNumber);
   }
+
   return result;
 }
 
