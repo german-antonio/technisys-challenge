a2f44e9262fb27d580f88693df3d01162b88dd63
German Antonio <german@tiralinea.com>
2021-03-12 05:23:40 -0300

    Added friendly user prompt

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 /* Dependencies */
+const prompt = require('prompt');
 const path = require('path');
+require('colors');
 
 /* Services */
 const getPrimeNumbers = require(path.join(process.cwd(), 'services', 'get-prime-numbers'))
 
-console.log(getPrimeNumbers(input));
+console.log('\n-- Technysis code challenge --\n'.cyan.bold);
+
+/**
+ * Prompt the user for a number between 2 and 100000, then print
+ * a descending list of all prime numbers starting from it
+ */
+(() => {
+  prompt.delimiter = '';
+  prompt.message = 'Please type a number\n'.cyan;
+
+  prompt.start();
+
+  return prompt.get([
+    {
+      name: 'input',
+      type: 'number',
+      required: true,
+      description: 'Max number:  '.bold.white
+    }
+  ],
+  (err, data) => {
+    if (err) {
+      console.log('\n\n  Cancelled\n'.bold.yellow);
+      process.exit(0);
+    }
+
+    try {
+      const numbers = getPrimeNumbers(data.input);
+      for (const number of numbers) {
+        console.log(number);
+      }
+      process.exit(0);
+    } catch (error) {
+      console.error(error.message);
+      process.exit(1);
+    }
+  });
+})();
