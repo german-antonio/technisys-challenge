3a970beaa3714bb15ad9fadc188393e0b03557c0
German Antonio <german@tiralinea.com>
2021-03-12 19:00:46 -0300

    Use inquirer instead of prompt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Dependencies */
-const prompt = require('prompt');
+const { prompt } = require('inquirer');
 const path = require('path');
 require('colors');
 
@@ -7,40 +7,29 @@ require('colors');
 const getPrimeNumbers = require(path.join(process.cwd(), 'services', 'get-prime-numbers'))
 
 console.log('\n-- Technysis code challenge --\n'.cyan.bold);
+console.log('\nGet a descending list of prime numbers starting from a max number\n'.cyan.bold);
 
 /**
  * Prompt the user for a number between 2 and 100000, then print
  * a descending list of all prime numbers starting from it
  */
-(() => {
-  prompt.delimiter = '';
-  prompt.message = 'Please type a number\n'.cyan;
-
-  prompt.start();
-
-  return prompt.get([
+(async () => {
+  const { maxNumber } = await prompt([
     {
-      name: 'input',
-      type: 'number',
-      required: true,
-      description: 'Max number:  '.bold.white
+      type:'number',
+      name: 'maxNumber',
+      message: 'Please type the max number:  '.bold.white,
     }
-  ],
-  (err, data) => {
-    if (err) {
-      console.log('\n\n  Cancelled\n'.bold.yellow);
-      process.exit(0);
-    }
+  ]);
 
-    try {
-      const numbers = getPrimeNumbers(data.input);
-      for (const number of numbers) {
-        console.log(number);
-      }
-      process.exit(0);
-    } catch (error) {
-      console.dir(error.message);
-      process.exit(1);
+  try {
+    const numbers = getPrimeNumbers(maxNumber);
+    for (const number of numbers) {
+      console.log(number);
     }
-  });
+    process.exit(0);
+  } catch (error) {
+    console.dir(error.message);
+    process.exit(1);
+  }
 })();
